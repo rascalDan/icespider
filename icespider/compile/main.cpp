da8945a802f42e8c6bb50c2a372d37771015114e
Dan Goodliffe <dan@randomdan.homeip.net>
2020-12-21 15:01:36 +0000

    Don't format program options

--- icespider/compile/main.cpp
+++ icespider/compile/main.cpp
@@ -10,9 +10,14 @@ main(int c, char ** v)
 	IceSpider::Compile::RouteCompiler rc;
 	std::filesystem::path input, output;
 	po::options_description opts("IceSpider compile options");
-	opts.add_options()("input", po::value(&input), "Input .json file")("output", po::value(&output),
-			"Output .cpp file")("include,I", po::value(&rc.searchPath)->composing(), "Search path")(
-			"help,h", po::value(&showHelp)->default_value(false)->zero_tokens(), "Help");
+	// clang-format off
+	opts.add_options()
+		("input", po::value(&input), "Input .json file")
+		("output", po::value(&output), "Output .cpp file")
+		("include,I", po::value(&rc.searchPath)->composing(), "Search path")
+		("help,h", po::value(&showHelp)->default_value(false)->zero_tokens(), "Help")
+		;
+	// clang-format on
 	po::positional_options_description pod;
 	pod.add("input", 1).add("output", 2);
 	po::variables_map vm;
