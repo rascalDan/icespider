49e804ec79b56bb107fedbeb6b47493e2401635e
Dan Goodliffe <dan@randomdan.homeip.net>
2023-07-30 12:08:32 +0100

    Inline functions for removing trailing/leading chars

--- icespider/core/util.cpp
+++ icespider/core/util.cpp
@@ -7,20 +7,4 @@ namespace IceSpider {
 	{
 		throw Http400_BadRequest();
 	}
-
-	void
-	remove_trailing(std::string_view & in, const char c)
-	{
-		if (const auto n = in.find_last_not_of(c); n != std::string_view::npos) {
-			in.remove_suffix(in.length() - n - 1);
-		}
-	}
-
-	void
-	remove_leading(std::string_view & in, const char c)
-	{
-		if (const auto n = in.find_first_not_of(c); n != std::string_view::npos) {
-			in.remove_prefix(n);
-		}
-	}
 }
--- icespider/core/util.h
+++ icespider/core/util.h
@@ -161,6 +161,19 @@ namespace IceSpider {
 		}
 	}
 
-	void remove_trailing(std::string_view & in, const char c);
-	void remove_leading(std::string_view & in, const char c);
+	inline void
+	remove_trailing(std::string_view & in, const char c)
+	{
+		if (const auto n = in.find_last_not_of(c); n != std::string_view::npos) {
+			in.remove_suffix(in.length() - n - 1);
+		}
+	}
+
+	inline void
+	remove_leading(std::string_view & in, const char c)
+	{
+		if (const auto n = in.find_first_not_of(c); n != std::string_view::npos) {
+			in.remove_prefix(n);
+		}
+	}
 }
